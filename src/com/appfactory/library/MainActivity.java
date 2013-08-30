d0f15b0203b55ce7e124e248ce79495fb9378cd2
yunpeijiao <107175341@qq.com>
2013-08-30 18:33:37 +0800

    Test Update
    update

--- src/com/appfactory/library/MainActivity.java
+++ src/com/appfactory/library/MainActivity.java
@@ -7,7 +7,6 @@ import com.appfactory.util.App;
 
 import android.os.Bundle;
 import android.os.SystemClock;
-import android.app.Activity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.util.Log;
